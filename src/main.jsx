13efc854c74bceef4dfc96bc0de29d91a6550251
Iswarya-S26 <emails2iswarya2602@gmail.com>
2025-03-03 22:32:02 +0000

    replace browserrouter with hashrouter for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={client}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
